64c73f25b012a7654855b72aac9ab15a786ad434
Derek Berger <derekberger734@gmail.com>
2025-05-13 22:07:25 -0400

    Add TFE backend

---
+++ tfe_cloud/backend.tf
@@ -0,0 +1,10 @@
+# Commented out for testing purposes but you would utilize a variation of this in production
+# terraform {
+#   backend "s3" {
+#     region   = "us-east-1"
+#     bucket   = "biotornic-use1develop-terraformcloud"
+#     key      = "terraform-cloud/terraform-infra-aws-shared"
+#     acl      = "private"
+#     role_arn = "arn:aws:iam::123445545:role/biotornic-develop-terraform"
+#   }
+# }
